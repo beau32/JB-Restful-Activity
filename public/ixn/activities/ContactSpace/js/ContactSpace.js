2d13f297a60e9843286bdb6f193045c5bb5a2a21
bo wang <bowang@bos-MacBook-Pro.local>
2020-07-14 17:01:22 +1200

    test

--- public/ixn/activities/ContactSpace/js/ContactSpace.js
+++ public/ixn/activities/ContactSpace/js/ContactSpace.js
@@ -11,61 +11,6 @@ define( function( require ) {
         connection.trigger('ready');
     })
 
-    function initialize (data) {
-        if (data) {
-            payload = data;
-        }
-    }
-
-	// This listens for Journey Builder to send tokens
-	// Parameter is either the tokens data or an object with an
-	// "error" property containing the error message
-	connection.on('getTokens', function( data ) {
-		if( data.error ) {
-			console.error( data.error );
-		} else {
-			tokens = data;
-		}
-	});
-
-	/**
-		If you want to have a multi-step configuration view, you need to manage the DOM manually.
-		You can filter what changes to make by implementing the following type of logic when Postmonger from the server triggers an "updateStep" call.
-		// connection.on('updateStep', step ) {
-
-			if( step  >= 1 && step <= 3 ) {
-				$('.step').hide(); // All DOM elements which are steps should have this class (this hides them all)
-				$('#step' + step ).show(); // This selectively only displays the current step
-				// Allow the user to make any changes and when you're ready, use:
-				connection.trigger( 'updateStep', step ); 
-			}
-		}
-	**/
-
-	// This listens for Journey Builder to send endpoints
-	// Parameter is either the endpoints data or an object with an
-	// "error" property containing the error message
-	connection.on('getEndpoints', function( data ) {
-		if( data.error ) {
-			console.error( data.error );
-		} else {
-			endpoints = data;
-		}
-	});
-
-    connection.on('requestPayload', function() {
-	 var payload = {};
- 
-        payload.options = {
-           
-        };
-
-		//TODO: Shouldn't this come from the data?
-        //payload.flowDisplayName = 'Hello World';
- 
-        connection.trigger('getPayload', payload);
-    });
-
 	// Journey Builder broadcasts this event to us after this module
 	// sends the "ready" method. JB parses the serialized object which
 	// consists of the Event Data and passes it to the
@@ -104,7 +49,7 @@ define( function( require ) {
                 options: data,
                 description: 'This is a configuration instance.'
             };
-            connection.trigger('updateActivity', uiPayload);
+            connection.trigger('save', uiPayload);
             //connection.trigger( 'save', uiPayload );
         }
     });
